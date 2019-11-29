33762a37396cb39a97e19a29f5c0371e87bbc7b4
Jay Smith <jayongitar@gmail.com>
2019-11-28 20:27:57 -0800

    stage objects and stage lookup tables complete and working with unit tests

--- scratch_base.py
+++ scratch_base.py
@@ -166,6 +166,9 @@ class mosfet:
         if self.type == 1 or self.type == 3:
             self.WL = 2*self.Id / ( self.upCox*self.Vov**2 )
             self.W  = self.WL*(self.type-1)
+        # check WL limits
+        if self.WL <= 2 or self.WL > 2000:
+            print(f'out of range: \n   Vov = {self.Vov} \n   Id = {self.Id} \n   WL={self.WL}')
         # gm = 2*Id/Vov
         self.gm = 2*self.Id/self.Vov
         self.gmp= 1.2*self.gm
@@ -269,32 +272,108 @@ class CG(mosfet):
         self.Rout = -1
         self.Cout = -1
         
+    def _print(self):
+        print('CG stage parameters:')
+        ee.print_A('   TI', 'ohms', self.TI)
+        ee.print_R('   Rin ', self.Rin)
+        ee.print_R('   Rout', self.Rout)
+        ee.print_C('   Cin ', self.Cin)
+        ee.print_C('   Cout', self.Cout)
+        
     def _set(self, Vov, Id, RL):
         self.RL    = RL
         self.M1.set_params(Vov, Id)
-        
         self.Rin  = 1/self.M1.gmp
         self.Rout = ee.parallel(2*self.M1.ro, self.RL)
         self.Cin  = self.M1.cgs + self.M1.csb
         self.Cout = self.M1.cgd + self.M1.cdb
         self.TI   = self.Rout
-        self._print()
         
-        return [self.TI, self.Rin, self.Cin, self.Rout, self.Cout]
+    def get_Rin(self):
+        return self.Rin
+    def get_Rout(self):
+        return self.Rout
+    def get_Cin(self):
+        return self.Cin
+    def get_Cout(self):
+        return self.Cout
+    def get_TI(self):
+        return self.TI
         
-    def _print(self):
-        print('CG stage parameters:')
-        ee.print_A('   A', 'V/A', self.TI)
-        ee.print_R('   Rin ', self.Rin)
-        ee.print_C('   Cin ', self.Cin)
-        ee.print_R('   Rout', self.Rout)
-        ee.print_C('   Cout', self.Cout)
     
-    
-cg_stage = CG()
-ret = cg_stage._set(0.2, 1E-4, 10000)
-print()
+def unit_test_CG():
+    print('--- CG unit test --------------')
+    cg = CG()
+    cg._set(0.2, 1E-4, 10000)
+    cg._print()
+
+
+unit_test_CG()
+
+#%% CG_lookup.  abstract CG stage to lookup3(Vov, Id, RL)
+class CG_LK(CG):
+    _Vov = np.linspace(0.15, 0.3,  5)
+    _Id  = np.linspace(1E-5, 1E-4, 5)
+    _RL  = np.linspace(1E+4, 4E+4, 5)
+#    print(f'_Vov: {_Vov}')
+#    print(f'_Id:  {_Id}')
+#    print(f'_RL:  {_RL}')
+    print()
+
+    def __init__(self):
+        super().__init__()
+        self.n_Vov = self._Vov.shape[0]
+        self.n_Id  = self._Id.shape[0]
+        self.n_RL= self._RL.shape[0]
+#        print(f'n_Vov: {self.n_Vov}')
+#        print(f'n_Id:  {self.n_Id}')
+#        print(f'n_RL:  {self.n_RL}')
         
+        self.Rin_data  = np.zeros((self.n_Vov, self.n_Id, self.n_RL))
+        self.Rout_data = np.zeros((self.n_Vov, self.n_Id, self.n_RL))
+        self.Cin_data  = np.zeros((self.n_Vov, self.n_Id, self.n_RL))
+        self.Cout_data = np.zeros((self.n_Vov, self.n_Id, self.n_RL))
+        self.TI_data   = np.zeros((self.n_Vov, self.n_Id, self.n_RL))
+        
+        for i in range(self.n_Vov):
+            for j in range(self.n_Id):
+                for k in range(self.n_RL):
+#                    print(f'i: {i}, j: {j}, k: {k}')
+                    super()._set(self._Vov[i], self._Id[j], self._RL[k])
+                    self.Rin_data [i,j,k] = super().get_Rin()
+                    self.Rout_data[i,j,k] = super().get_Rout()
+                    self.Cin_data [i,j,k] = super().get_Cin()
+                    self.Cout_data[i,j,k] = super().get_Cout()
+                    self.TI_data  [i,j,k] = super().get_TI()
+        self.Rin_f  = RegularGridInterpolator((self._Vov, self._Id, self._RL), self.Rin_data)
+        self.Rout_f = RegularGridInterpolator((self._Vov, self._Id, self._RL), self.Rout_data)
+        self.Cin_f  = RegularGridInterpolator((self._Vov, self._Id, self._RL), self.Cin_data)
+        self.Cout_f = RegularGridInterpolator((self._Vov, self._Id, self._RL), self.Cout_data)
+        self.TI_f   = RegularGridInterpolator((self._Vov, self._Id, self._RL), self.TI_data)
+                
+    def get_Rin(self, Vov, Id, RL):
+        return self.Rin_f([Vov, Id, RL])
+    def get_Rout(self, Vov, Id, RL):
+        return self.Rout_f([Vov, Id, RL])
+    def get_Cin(self, Vov, Id, RL):
+        return self.Cin_f([Vov, Id, RL])
+    def get_Cout(self, Vov, Id, RL):
+        return self.Cout_f([Vov, Id, RL])
+    def get_TI(self, Vov, Id, RL):
+        return self.TI_f([Vov, Id, RL])
+
+def CG_LK_unit_test(Vov, Id, RL):
+    print('--- CG_LK_unit_test ----------------')
+    cg = CG_LK()
+    ee.print_A('   TI', 'ohms', cg.get_TI  (Vov, Id, RL))
+    ee.print_R('   Rin ', cg.get_Rin (Vov, Id, RL))
+    ee.print_R('   Rout', cg.get_Rout(Vov, Id, RL))
+    ee.print_C('   Cin ', cg.get_Cin (Vov, Id, RL))
+    ee.print_C('   Cout', cg.get_Cout(Vov, Id, RL))
+    print('------------------------------------\n')
+    
+CG_LK_unit_test(0.2, 1E-4, 10000)
+
 
 #%% CS
 class CS(mosfet):
@@ -306,32 +385,108 @@ class CS(mosfet):
         self.Cin  = -1
         self.Rout = -1
         self.Cout = -1
-#        self.M.print_all()
+
+    def _print(self):
+        print('CS stage parameters:')
+        ee.print_A('   |A| ', 'V/V', self.A1)
+        ee.print_R('   Rin ', self.Rin)
+        ee.print_R('   Rout', self.Rout)
+        ee.print_C('   Cin ', self.Cin)
+        ee.print_C('   Cout', self.Cout)
         
-    def _set(self, Vov, Id, gain):  # A = -gain
-        self.A1 = -gain
+    def _set(self, Vov, Id, A1):  # A = -gain
+        self.A1 = A1
         self.M2.set_params(Vov, Id)
         
         self.Rin  = 1E+21
         self.Rout = ee.parallel(2*self.M2.ro, 1/(self.A1*self.M2.gmp))
         self.Cin  = self.M2.cgs + self.M2.cgb + (1+self.A1)*self.M2.cgd
         self.Cout = (1+1/self.A1)*self.M2.cgd + self.M2.cdb
-        self._print()
+#        self._print()
         
-        return [self.A1, self.Rin, self.Cin, self.Rout, self.Cout]
+    def get_Rin(self):
+        return self.Rin
+    def get_Rout(self):
+        return self.Rout
+    def get_Cin(self):
+        return self.Cin
+    def get_Cout(self):
+        return self.Cout
+    def get_A1(self):
+        return self.A1
+
+def unit_test_CS():
+    print('--- CS_unit_test --------------------')
+    cs = CS()
+    cs._set(0.2, 1E-4, 3)
+    cs._print()
+    print()
+
+unit_test_CS()
+
+
+#%%
+class CS_LK(CS):
+    _Vov = np.linspace(0.15, 0.3,  5)
+    _Id  = np.linspace(1E-5, 1E-4, 5)
+    _A1  = np.linspace(1, 5, 5)
+#    print(f'_Vov: {_Vov}')
+#    print(f'_Id:  {_Id}')
+#    print(f'_gain:{_gain}')
+    
+    def __init__(self):
+        super().__init__()
+        self.n_Vov = self._Vov.shape[0]
+        self.n_Id  = self._Id.shape[0]
+        self.n_A1= self._A1.shape[0]
+        print(f'n_Vov: {self.n_Vov}')
+        print(f'n_Id:  {self.n_Id}')
+        print(f'n_A1:  {self.n_A1}')
         
-    def _print(self):
-        print('CS stage parameters:')
-        ee.print_A('   A', 'V/V', self.A1)
-        ee.print_R('   Rin ', self.Rin)
-        ee.print_C('   Cin ', self.Cin)
-        ee.print_R('   Rout', self.Rout)
-        ee.print_C('   Cout', self.Cout)
+        self.Rin_data  = np.zeros((self.n_Vov, self.n_Id, self.n_A1))
+        self.Rout_data = np.zeros((self.n_Vov, self.n_Id, self.n_A1))
+        self.Cin_data  = np.zeros((self.n_Vov, self.n_Id, self.n_A1))
+        self.Cout_data = np.zeros((self.n_Vov, self.n_Id, self.n_A1))
+        self.A1_data   = np.zeros((self.n_Vov, self.n_Id, self.n_A1))
+        
+        for i in range(self.n_Vov):
+            for j in range(self.n_Id):
+                for k in range(self.n_A1):
+#                    print(f'i: {i}, j: {j}, k: {k}')
+                    super()._set(self._Vov[i], self._Id[j], self._A1[k])
+                    self.Rin_data [i,j,k] = super().get_Rin()
+                    self.Rout_data[i,j,k] = super().get_Rout()
+                    self.Cin_data [i,j,k] = super().get_Cin()
+                    self.Cout_data[i,j,k] = super().get_Cout()
+                    self.A1_data  [i,j,k] = super().get_A1()
+        self.Rin_f  = RegularGridInterpolator((self._Vov, self._Id, self._A1), self.Rin_data)
+        self.Rout_f = RegularGridInterpolator((self._Vov, self._Id, self._A1), self.Rout_data)
+        self.Cin_f  = RegularGridInterpolator((self._Vov, self._Id, self._A1), self.Cin_data)
+        self.Cout_f = RegularGridInterpolator((self._Vov, self._Id, self._A1), self.Cout_data)
+        self.A1_f   = RegularGridInterpolator((self._Vov, self._Id, self._A1), self.A1_data)
+                
+    def get_Rin(self, Vov, Id, A1):
+        return self.Rin_f ([Vov, Id, A1])
+    def get_Rout(self, Vov, Id, A1):
+        return self.Rout_f([Vov, Id, A1])
+    def get_Cin(self, Vov, Id, A1):
+        return self.Cin_f ([Vov, Id, A1])
+    def get_Cout(self, Vov, Id, A1):
+        return self.Cout_f([Vov, Id, A1])
+    def get_A1(self, Vov, Id, A1):
+        return self.A1_f([Vov, Id, A1])
     
+def CS_LK_unit_test():
+    print('--- CS_LK_unit_test ----------------')
+    cs = CS_LK()
+    ee.print_A('   |A| ', 'V/V', cs.get_A1(0.2, 1E-4, 3))
+    ee.print_R('   Rin ', cs.get_Rin(0.2,  1E-4, 3))
+    ee.print_R('   Rout', cs.get_Rout(0.2, 1E-4, 3))
+    ee.print_C('   Cin ', cs.get_Cin(0.2,  1E-4, 3))
+    ee.print_C('   Cout', cs.get_Cout(0.2, 1E-4, 3))
+    print('------------------------------------\n')
     
-cs_stage = CS()
-ret = cs_stage._set(0.2, 1E-5, -4)
-print()
+CS_LK_unit_test()
     
 
 #%% CD
@@ -344,33 +499,110 @@ class CD(mosfet):
         self.Cin  = -1
         self.Rout = -1
         self.Cout = -1
-#        self.M.print_all()
+        
+    def _print(self):
+        print('CD stage parameters:')
+        ee.print_A('    |A| ', 'V/V', -self.A2)
+        ee.print_R('    Rin ', self.Rin)
+        ee.print_R('    Rout', self.Rout)
+        ee.print_C('    Cin ', self.Cin)
+        ee.print_C('    Cout', self.Cout)
         
     def _set(self, Vov, Id): 
         self.M3.set_params(Vov, Id)
-        
         self.Rin  = 1E+21
-        self.Rout = ee.parallel(2*self.M3.ro, 0.84/2*self.M3.Vov/self.M3.Id)
+        self.Rout = ee.parallel(2*self.M3.ro, 1/self.M3.gmp)
         self.Cin  = self.M3.cgs + self.M3.cgb + (1+self.A2)*self.M3.cgd
         self.Cout = (1+1/self.A2)*self.M3.cgd + self.M3.cdb
-        self._print()
-        
-        return [self.A2, self.Rin, self.Cin, self.Rout, self.Cout]
-        
-    def _print(self):
-        print('CS stage parameters:')
-        ee.print_A('   A', 'V/V', self.A2)
-        ee.print_R('   Rin ', self.Rin)
-        ee.print_C('   Cin ', self.Cin)
-        ee.print_R('   Rout', self.Rout)
-        ee.print_C('   Cout', self.Cout)
+#        self._print()
     
-    
-cd_stage = CD()
-ret = cd_stage._set(0.2, 1E-5)
-print()
+    def get_A2(self):
+        return self.A2
+    def get_Rin(self):
+        return self.Rin
+    def get_Rout(self):
+        return self.Rout
+    def get_Cin(self):
+        return self.Cin
+    def get_Cout(self):
+        return self.Cout
+  
+def unit_test_CD():
+    print('--- CD_unit_test ---------------------')
+    cd = CD()
+    cd._set(0.2, 1E-4)
+    cd._print()
+    print('')
+
+unit_test_CD()
 
 
+#%%
+class CD_LK(CD):
+    _Vov = np.linspace(0.15, 0.3,  5)
+    _Id  = np.linspace(1E-5, 1E-4, 5)
+#    print(f'_Vov: {_Vov}')
+#    print(f'_Id:  {_Id}')
+    
+    def __init__(self):
+        super().__init__()
+        self.n_Vov = self._Vov.shape[0]
+        self.n_Id  = self._Id.shape[0]
+        self.A2_data   = np.zeros((self.n_Vov, self.n_Id))
+        self.Rin_data  = np.zeros((self.n_Vov, self.n_Id))
+        self.Rout_data = np.zeros((self.n_Vov, self.n_Id))
+        self.Cin_data  = np.zeros((self.n_Vov, self.n_Id))
+        self.Cout_data = np.zeros((self.n_Vov, self.n_Id))
+        print(f'n_Vov: {self.n_Vov}')
+        print(f'n_Id:  {self.n_Id}')
+        for i in range(self.n_Vov):
+            for j in range(self.n_Id):
+#                print(f'i: {i}, j: {j}')
+                super()._set(self._Vov[i], self._Id[j])
+                self.A2_data  [i,j] = super().get_A2()
+                self.Rin_data[i,j]  = super().get_Rin()
+                self.Rout_data[i,j] = super().get_Rout()
+                self.Cin_data[i,j]  = super().get_Cin()
+                self.Cout_data[i,j] = super().get_Cout()
+        self.A2_f   = RegularGridInterpolator((self._Vov, self._Id), self.A2_data)
+        self.Rin_f  = RegularGridInterpolator((self._Vov, self._Id), self.Rin_data)
+        self.Rout_f = RegularGridInterpolator((self._Vov, self._Id), self.Rout_data)
+        self.Cin_f  = RegularGridInterpolator((self._Vov, self._Id), self.Cin_data)
+        self.Cout_f = RegularGridInterpolator((self._Vov, self._Id), self.Cout_data)
+    
+    def get_A2(self, Vov, Id):
+        return self.A2_f([Vov, Id])
+    def get_Rin(self, Vov, Id):
+        return self.Rin_f([Vov, Id])
+    def get_Rout(self, Vov, Id):
+        return self.Rout_f([Vov, Id])
+    def get_Cin(self, Vov, Id):
+        return self.Cin_f([Vov, Id])
+    def get_Cout(self, Vov, Id):
+        return self.Cout_f([Vov, Id])
+        
+def CD_LK_unit_test():
+    print('--- CD_LK_unit_test ----------------')
+    cd = CD_LK()
+    ee.print_A('   |A| ', 'V/V', -cd.get_A2(0.2, 1E-4))
+    ee.print_R('   Rin ', cd.get_Rin(0.2, 1E-4))
+    ee.print_R('   Rout', cd.get_Rout(0.2, 1E-4))
+    ee.print_C('   Cin ', cd.get_Cin(0.2, 1E-4))
+    ee.print_C('   Cout', cd.get_Cout(0.2, 1E-4))
+    print('------------------------------------')
+    
+CD_LK_unit_test()
+
+##%% TIA
+#class TIA(CG, CS, CD):
+#    def __init__(self):
+#        self.CG = CG()
+#        self.CS = CS()
+#        self.CD = CD()
+#        self.Cin  = 100
+#        self.Rout = 1E+4
+#        self.Cout = 500
+        
 
 
 
